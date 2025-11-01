60d4cc4f4dc9b3c1e9acdbf63c30e1bf024f389c
Victor Alfonso Colina Leon <VictorColina@MacBook-Pro-de-mb-24-csvc10.local>
2025-10-31 22:41:28 -0300

    fix: adjust colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,8 @@ return {
   config = function()
     vim.g.everforest_background = "hard"
     vim.g.everforest_transparent_background = 2
+    vim.g.everforest_float_style = "dim"
+    vim.g.everforest_better_performance = 1
     vim.cmd.colorscheme("everforest")
   end,
 }
